d23df9792b4a9973629a4e45d6026ee258bb3af5
karan Ainkar <karanainkar7@gmail.com>
2025-06-16 10:55:34 +0530

    done

--- src/Functions/ApiFetch.js
+++ src/Functions/ApiFetch.js
@@ -2,7 +2,6 @@ async function getWeatherAPIData(city) {
   try {
     let apiKey = import.meta.env.VITE_WEATHER_API_ID;
      const url = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=5&aqi=yes`;
-     console.log(url);
      const response = await fetch(url);
      const data = await response.json();
     return data;
